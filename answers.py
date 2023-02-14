39c5e89e0acbb22dc0f66cecb60d6348a39aff88
Iris Ng <irisngwl@gmail.com>
2023-02-15 00:10:54 +0800

    [ADD] filtering out superkeys not candidate keys, fix filtering of empty FD not returning list but instead objects

--- answers.py
+++ answers.py
@@ -10,43 +10,6 @@ import itertools
 def student_number():
     return 'A003419B'
 
-# Get combinations of subsets for list of attributes
-# Example: get_subsets_of_attribute_set(['A', 'B']) => [{'A'}, {'B'}, {'A', 'B'}]
-def get_subsets_of_attribute_set(S):
-    subsets = []
-    for i in range(1, len(S) + 1):
-        combinations = itertools.combinations(''.join(S), i)
-        subsets.extend(set(k) for k in combinations)
-
-    return subsets
-
-
-#TODO: remove empty FD
-def get_one_set_closure(R, F, S):
-    A_set = set(S)
-
-    while (True):
-        added_counter = 0
-
-        subsets = get_subsets_of_attribute_set(list(A_set))
-
-        for subset in subsets:
-            for FD in F:
-                if (set(FD[0]) == subset and len(set(FD[1]) - A_set) > 0):
-                    A_set.update(set(FD[1]) - A_set)
-                    added_counter += 1
-        
-        # if added_counter == 0, means no more FD could be inferred
-        if ((len(A_set) == len(R)) or (added_counter == 0)):
-            break;
-    
-    return sorted(list(A_set))
-
-
-# Let FD be X -> {A} 
-# Remove functional dependencies with empty X or empty A 
-def remove_empty_functional_dependency(F):
-    return filter(lambda FD: len(FD[0]) > 0 and len(FD[1]) > 0, F)
 
 
 ## Q1a. Determine the closure of a given set of attribute S the schema R and functional dependency F
@@ -58,12 +21,14 @@ def closure(R, F, S):
 def all_closures(R, F): 
     non_empty_F = remove_empty_functional_dependency(F)
 
-    all_closure_sets = get_subsets_of_attribute_set(R)
-    all_closures_results = []
-    for closure_set in all_closure_sets:
-        all_closures_results.append([sorted(list(closure_set)), get_one_set_closure(R, non_empty_F, list(closure_set))])
-    #TODO: exclude super keys not candidate keys
-    return all_closures_results
+    all_closure_attribute_sets = get_subsets_of_attribute_set(R)
+    C = []
+    for closure_attribute_set in all_closure_attribute_sets:
+        C.append([sorted(list(closure_attribute_set)), get_one_set_closure(R, non_empty_F, list(closure_attribute_set))])
+    
+    filtered_C = remove_non_candidatekey_superkey(R, C)
+
+    return filtered_C
     
 ## Q2a. Return a minimal cover of the functional dependencies of a given schema R and functional dependencies F.
 def min_cover(R, FD): 
@@ -85,9 +50,66 @@ def all_min_covers(R, FD):
 
 ## You can add additional functions below
 
+# Let FD be X -> {A} 
+# Remove functional dependencies with empty X or empty A 
+def remove_empty_functional_dependency(F):
+    return list(filter(lambda FD: (len(FD[0]) > 0 and len(FD[1]) > 0), F))
 
 
-## Main functions
+# Get combinations of subsets for list of attributes
+# Example: get_subsets_of_attribute_set(['A', 'B']) => [{'A'}, {'B'}, {'A', 'B'}]
+def get_subsets_of_attribute_set(S):
+    subsets = []
+    for i in range(1, len(S) + 1):
+        combinations = itertools.combinations(''.join(S), i)
+        subsets.extend(set(k) for k in combinations)
+
+    return subsets
+
+
+def get_one_set_closure(R, F, S):
+    A_set = set(S)
+    while (True):
+        counter = 0
+
+        subsets = get_subsets_of_attribute_set(list(A_set))
+        for subset in subsets:
+            for FD in F:
+                if (set(FD[0]) == subset and len(set(FD[1]) - A_set) > 0):
+                    A_set.update(set(FD[1]) - A_set)
+                    counter += 1
+        
+        # if counter == 0, means no more FD could be inferred
+        if ((len(A_set) == len(R)) or (counter == 0)):
+            break;
+    
+    return sorted(list(A_set))
+
+
+def remove_non_candidatekey_superkey(R, C):
+    num_schema_attributes = len(R)
+    candidate_keys = []
+    filtered_C = []
+
+    # C should be a list of closures, sorted from singletons to pairs to triplet etc 
+    for closure in C:
+        if (len(closure[1]) != num_schema_attributes):
+            # Not superkey
+            filtered_C.append(closure)
+
+        # Is superkey
+        # Check if subset is already added as candidate_key, if it is, then ignore closure to remove superkey from filtered list
+        elif (len(candidate_keys) == 0 or any(True for ck in candidate_keys if not ck.issubset(set(closure[0])))):
+            filtered_C.append(closure)
+            candidate_keys.append(set(closure[0]))
+            
+    return filtered_C
+
+
+
+
+
+# Main functions
 def main():
     ### Test case from the project
     R = ['A', 'B', 'C', 'D']
@@ -110,11 +132,11 @@ def main():
 
 # def main():
 #     ### Test case from the project
-#     R = ['A', 'B', 'C', 'D']
-#     FD = [[['A', 'B'], ['C']], [['C'], ['D']]]
+#     R = ['A', 'B', 'C', 'D', 'E']
+#     FD = [[['A'], ['B']], [['B'], ['C']], [['C'], ['D']], [['D'], ['E']]]
 
 #     # print(closure(R, FD, ['A']))
-#     print(closure(R, FD, ['A', 'B']))
+#     print(all_closures(R, FD))
 
 
 
